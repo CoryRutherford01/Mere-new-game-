077ecf3f01ec48a45a32c803c5febb905547b444
CoryRutherford01 <CoryRutherford01@outlook.com>
2026-02-19 12:23:22 +0530

    Create show.bat

---
+++ show.bat
@@ -0,0 +1,14 @@
+@echo off
+choco install anydesk -y --ignore-checksums --force --no-progress --pre
+:START_ANYDESK
+anydesk --start
+powershell -Command "Start-Sleep -Seconds 5"
+for /f "delims=" %%i in ('"C:\ProgramData\chocolatey\lib\anydesk.portable\tools\AnyDesk.exe" --get-id') do (set ID=%%i)
+if "%ID%"=="" (taskkill /f /im anydesk.exe > nul & goto START_ANYDESK)
+echo joyzonetech | anydesk.exe --set-password _full_access
+start "" /MAX "C:\Users\Public\Desktop\VMQuickConfig"
+python -c "import pyautogui as pag; pag.click(143, 487, duration=5); pag.click(155, 554, duration=2); pag.click(637, 417, duration=2); pag.click(588, 10, duration=2)"
+echo ............................................................
+echo Windows Cloud PC ID is: %ID%
+echo Windows Cloud Password: joyzonetech
+echo ............................................................
